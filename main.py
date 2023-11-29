b0a908867cf33f3356c11f671b31848ed648a44c
Jon Lillie <34103558+jolillie@users.noreply.github.com>
2023-11-29 19:24:32 +0000

    updates to main and added in schema and test files

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import logging
+import sys
+import yaml
+
+try:
+    from yaml import CFullLoader as Loader
+except ImportError:
+    from yaml import FullLoader as Loader
+
+from cerberus import Validator
+from distutils.util import strtobool
+from pathlib import Path
 
 
+
+
+
+if __name__ == "__main__"
+  main()
